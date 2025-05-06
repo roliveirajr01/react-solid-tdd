66d61a87469be84819779dfca1171536db54f7db
Rogerio Oliveira <roliveirajr011@gmail.com>
2025-05-06 20:01:09 -0300

    feat: add authentication usecase

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
-  ],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node'
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  }
 }
